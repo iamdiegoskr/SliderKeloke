8c8258765bd8d5c96a647952e8317734f2577294
diego martinez <juandiego337@hotmail.com>
2021-02-12 13:41:02 -0500

    function nextImage is implemented with logic

---
+++ slider.js
@@ -0,0 +1,32 @@
+const slider = document.querySelector('#slider');
+const sliderSections = document.querySelectorAll('.slider__section');
+const sliderSectionLast = sliderSections[sliderSections.length - 1];
+
+const btnRigth = document.querySelector('#btn-slider-rigth');
+const btnLeft = document.querySelector('#btn-slider-left');
+
+
+slider.insertAdjacentElement('afterbegin', sliderSectionLast) //El ultimo sliderSection al principio del slider
+
+function nextBanner() {
+    let sliderSectionFirst = document.querySelectorAll('.slider__section')[0];
+
+    slider.style.marginLeft = '-200%'
+    slider.style.transition = 'all .5s'
+
+    setTimeout(() => {
+        slider.style.transition = 'none'
+        slider.insertAdjacentElement('beforeend', sliderSectionFirst)
+        slider.style.marginLeft = '-100%'
+    }, 500);
+
+
+}
+
+function previousBanner() {
+
+}
+
+
+btnRigth.addEventListener('click', nextBanner)
+btnLeft.addEventListener('click', previousBanner)
